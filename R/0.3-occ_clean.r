5f8f5dfa5aa9ff53ef63b154d79094586d3dfc6b
Lukas Prader <lukasprader77@gmail.com>
2023-08-03 16:47:11 +0200

    outline rough intro + methods

--- R/0.3-occ_clean.r
+++ R/0.3-occ_clean.r
@@ -4,20 +4,26 @@ library(CoordinateCleaner)
 
 ## clean occurence presence data
 # load raw gbif data
-axyridis_raw <- read.csv("R/data/Harmonia-axyridis_gbif_raw.csv",
+axyridis_raw <- read.csv("R/data/occurence_data/Harmonia-axyridis_gbif_raw.csv",
   header = TRUE,
   sep = "\t"
 )
 # remove relevant NAs in data
-axyridis <- subset(axyridis_raw, !is.na(decimalLongitude & decimalLatitude & year &
-  coordinateUncertaintyInMeters) & year < 2023 &
-  coordinateUncertaintyInMeters < 1000)
+axyridis <- subset(axyridis_raw, !is.na(decimalLongitude & decimalLatitude & 
+    year & coordinateUncertaintyInMeters) & year < 2023 &
+    coordinateUncertaintyInMeters < 1001)
+
+# subset data into eu and as spatial extent for further cleaning
+
 # clean coordinates using CoordinateCleaner
 axyridis_clean <- clean_coordinates(axyridis,
   lat = "decimalLatitude",
   lon = "decimalLongitude",
   countries = "countryCode", capitals_rad = 5000
 )
-# save data
-saveRDS(axyridis_clean, file = "R/data/axyridis_global.rds")
+
+
+# merge back together and save
+
+saveRDS(axyridis_clean, file = "R/data/occurence_data/axyridis_clean.rds")
 print("completed occurence cleaning")
