9b0e2d6b0b75ba61e44e678a2def85ef2a0ba330
Lukas Prader <lukasprader77@gmail.com>
2023-08-22 12:22:15 +0200

    native vs eu comp with total instead of yearly

--- R/1.4-niche_comp.r
+++ R/1.4-niche_comp.r
@@ -1,4 +1,4 @@
-# this file computes and visualizes niche comparisons according to 
+# this file computes and visualizes niche comparisons according to
 # (Broennimann et al. 2011), using the `ecospat` library
 
 # used libraries
@@ -19,7 +19,7 @@ bio_tot <- select(pa, starts_with("bio")) # only use bioclim vars
 # pca calibrated on all datapoints
 pca_tot <- dudi.pca(bio_tot, scannf = FALSE, nf = 2)
 # plot pca axis contribution
-fname = "R/plots/niche_comp/as_eu_pca.png"
+fname <- "R/plots/niche_comp/as_eu_pca.png"
 png(width = 600, height = 600, filename = fname)
 ecospat.plot.contrib(contrib = pca_tot$co, eigen = pca_tot$eig)
 dev.off()
@@ -29,84 +29,74 @@ scores_tot <- pca_tot$li
 scores_po_as <- suprow(pca_tot, select(po_as, starts_with("bio")))$li
 # pca scores for native occs with background
 scores_pa_as <- suprow(pca_tot, select(pa_as, starts_with("bio")))$li
+# pca scores for eu occs
+scores_po_eu <- suprow(pca_tot, select(po_eu, starts_with("bio")))$li
+# pca scores for eu occs with background
+scores_pa_eu <- suprow(pca_tot, select(pa_eu, starts_with("bio")))$li
 
-# calculate native dynamic occurrence densities grid
+# calculate native dynamic occurrence densities grids
 grid_as <- ecospat.grid.clim.dyn(
     glob = scores_tot,
     glob1 = scores_pa_as, sp = scores_po_as, R = 100, th.sp = 0
 )
+grid_eu <- ecospat.grid.clim.dyn(
+    glob = scores_tot,
+    glob1 = scores_pa_eu, sp = scores_po_eu, R = 100, th.sp = 0
+)
 
-# calculate overlap between total native and each EU year
-for (y in 2002:2022) {
-    # take new subsets
-    pa_eu_y <- subset(pa_eu, Year == y)
-    po_eu_y <- subset(po_eu, Year == y)
-    # pca scores for EU
-    scores_po_eu_y <- suprow(pca_tot, select(po_eu_y, starts_with("bio")))$li
-    scores_pa_eu_y <- suprow(pca_tot, select(pa_eu_y, starts_with("bio")))$li
-    # EU grid
-    grid_eu_y <- ecospat.grid.clim.dyn(
-        glob = scores_tot,
-        glob1 = scores_pa_eu_y, sp = scores_po_eu_y, R = 100, th.sp = 0
-    )
-    # overlap
-    ol <- ecospat.niche.overlap(grid_as, grid_eu_y, cor = TRUE)$D
-    # choose color for overlap
-    if (ol == 0) {
-        col_stab <- "green"
-    } else {
-        col_stab <- "blue"
-    }
-    # plot niche overlap
-    fname = paste0("R/plots/niche_comp/as_eu_", y, "_niche.png")
-    png(width = 600, height = 600, filename = fname)
-    text <- paste("Niche Overlap native / EU", y, "| D:", ol)
-    ecospat.plot.niche.dyn(grid_as, grid_eu_y,
-        quant = 0.1,
-        col.unf = "green", col.exp = "red", col.stab = col_stab,
-        interest = 2, title = text, name.axis1 = "PC1",
-        name.axis2 = "PC2"
-    )
-    dev.off()
+# compute niche overlap (Schoeners overlap metric)
+ol <- ecospat.niche.overlap(grid_as, grid_eu, cor = TRUE)$D
 
-    fname = paste0("R/plots/niche_comp/as_eu_", y, "_eq-sim.png")
-    png(width = 1200, height = 600, filename = fname)
-    par(mfrow = c(1,2))
-    # niche equivalency test
-    eq_test <- ecospat.niche.equivalency.test(grid_as, grid_eu_y,
-        rep = 100, ncores = 2 # , alternative = "greater"
-    )
-    # plot eq_test
-    ecospat.plot.overlap.test(eq_test, "D", paste("Equivalency Native / EU", y))
-    rm(eq_test)
-    # niche similarity test
-    sim_test <- ecospat.niche.similarity.test(grid_as, grid_eu_y,
-        rep = 100, rand.type = 2, ncores = 2 # , alternative
-    )
-    # plot sim_test
-    ecospat.plot.overlap.test(sim_test, "D", paste("Similarity Native / EU", y))
-    rm(sim_test)
-    dev.off()
-    rm(list = c("grid_eu_y1", "grid_eu_y2"))
-}
-cat("compared EU years to native range \n")
-rm(list = c('pca_tot', 'scores_tot', 'scores_pa_as', 'scores_po_as', 'grid_as'))
+# plot niche overlap
+fname <- "R/plots/niche_comp/as_eu_niche.png"
+png(width = 600, height = 600, filename = fname)
+text <- paste("Niche Overlap native / EU | D:", ol)
+ecospat.plot.niche.dyn(grid_as, grid_eu,
+    quant = 0.1,
+    col.unf = "green", col.exp = "red", col.stab = "blue",
+    interest = 2, title = text, name.axis1 = "PC1",
+    name.axis2 = "PC2"
+)
+dev.off()
+
+fname <- "R/plots/niche_comp/as_eu_eq-sim.png"
+png(width = 1200, height = 600, filename = fname)
+par(mfrow = c(1, 2))
+# niche equivalency test
+eq_test <- ecospat.niche.equivalency.test(grid_as, grid_eu,
+    rep = 100, ncores = 2 # , alternative = "greater"
+)
+# plot eq_test
+ecospat.plot.overlap.test(eq_test, "D", "Equivalency Native / EU")
+rm(eq_test)
+# niche similarity test
+sim_test <- ecospat.niche.similarity.test(grid_as, grid_eu,
+    rep = 100, rand.type = 2, ncores = 2 # , alternative
+)
+# plot sim_test
+ecospat.plot.overlap.test(sim_test, "D", "Similarity Native / EU")
+rm(sim_test)
+dev.off()
+rm(list = c("grid_as", "grid_eu"))
+
+cat("compared EU to native range \n")
+rm(list = c("pca_tot", "scores_tot", "scores_pa_as", "scores_po_as"))
 gc()
 
 
 ## compare EU years to their following year
-bio_eu = select(pa_eu, starts_with("bio"))
+bio_eu <- select(pa_eu, starts_with("bio"))
 # pca calibrated on eu datapoints
 pca_eu <- dudi.pca(bio_eu, scannf = FALSE, nf = 2)
 # plot pca axis contribution
-fname = "R/plots/niche_comp/eu_years_pca.png"
+fname <- "R/plots/niche_comp/eu_years_pca.png"
 png(width = 600, height = 600, filename = fname)
 ecospat.plot.contrib(contrib = pca_eu$co, eigen = pca_eu$eig)
 dev.off()
 # pca scores for the whole area
 scores_eu_tot <- pca_eu$li
 
-#values for starting year
+# values for starting year
 pa_eu_y2 <- subset(pa_eu, Year == 2002)
 po_eu_y2 <- subset(po_eu, Year == 2002)
 scores_po_eu_y2 <- suprow(pca_eu, select(po_eu_y2, starts_with("bio")))$li
@@ -143,7 +133,7 @@ for (y in 2003:2022) {
         col_stab <- "blue"
     }
     # plot niche overlap
-    fname = paste0("R/plots/niche_comp/eu_", y - 1, y, "_niche.png")
+    fname <- paste0("R/plots/niche_comp/single_ys/eu_", y - 1, y, "_niche.png")
     png(width = 600, height = 600, filename = fname)
     text <- paste("Niche Overlap EU", y - 1, "/", y, "| D:", ol)
     ecospat.plot.niche.dyn(grid_eu_y1, grid_eu_y2,
@@ -154,9 +144,9 @@ for (y in 2003:2022) {
     )
     dev.off()
 
-    fname = paste0("R/plots/niche_comp/eu_", y - 1, y, "_eq-sim.png")
+    fname <- paste0("R/plots/niche_comp/single_ys/eu_", y - 1, y, "_eq-sim.png")
     png(width = 1200, height = 600, filename = fname)
-    par(mfrow = c(1,2))
+    par(mfrow = c(1, 2))
     # niche equivalency test
     eq_test <- ecospat.niche.equivalency.test(grid_eu_y1, grid_eu_y2,
         rep = 100, ncores = 2 # , alternative = "greater"
